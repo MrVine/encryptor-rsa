90f9c3c7a9e3fe6690cbf0de53e772914bd62888
vitalii <porshegt4@gmail.com>
2018-06-24 03:03:50 +0300

    args parsing logic was changed
    [cmd] argument was separated to [command] and [message] arguments. 
    print parsed arguments was added for debug purposes.
    print parsed arguments was commented.
    check correctness of [keyLength] argument was fixed.

--- cmd/args.go
+++ cmd/args.go
@@ -1,13 +1,14 @@
 package main
 
-import (
+import
+(
 	"flag"
 	"os"
 	"strconv"
 	rsa "github.com/mrvine/encryptor-rsa"
 	"crypto/rand"
 	"fmt"
-	"strings"
+	"errors"
 )
 
 type RsaArgs struct {
@@ -20,6 +21,18 @@ type RsaArgs struct {
 }
 
 func getArgs() (args RsaArgs, err error) {
+
+	command := flag.String(
+		"command",
+		"",
+		`command to do (generate/encrypt/decrypt)`,
+	)
+
+	message := flag.String(
+		"message",
+		"",
+		`message to encrypt/decrypt`,
+	)
 
 	keyLengthStr := flag.String(
 		"keyLength",
@@ -45,36 +58,58 @@ func getArgs() (args RsaArgs, err error) {
 		"password to file with private key",
 	)
 
-	cmd := flag.String("cmd", "", `Usage:
-		encrypt <message>
-		decrypt <message>
-	`)
-
 	flag.Parse()
 
-	keyLength, err := strconv.Atoi(*keyLengthStr)
+	/*
+	/* Can be uncommented for debug purposes
+	/*
+	fmt.Println("command:", *command)
+	fmt.Println("message:", *message)
+	fmt.Println("key-length: ", *keyLengthStr)
+	fmt.Println("public-key-file:", *publicKeyFile)
+	fmt.Println("private-key-file:", *privateKeyFile)
+	fmt.Println("private-key-password:", *privateKeyPassword)
+	*/
+
+	if *command == "" || (*command != "generate" && *message == "") {
+		err = errors.New("[command] and [message] arguments are required")
+		return
+	}
+
+	if *command == "encrypt" && *publicKeyFile == "" {
+		err = errors.New("you should specify public key to encrypt")
+		return
+	}
+
+	if *command == "decrypt" && *privateKeyFile == "" {
+		err = errors.New("you should specify private key to decrypt")
+		return
+	}
+
+	if *keyLengthStr == "" {
+		*keyLengthStr = "0"
+	}
+
+	keyLength32, err := strconv.ParseInt(*keyLengthStr, 10, 32)
+	keyLength := int(keyLength32)
 	if err != nil || !rsa.IsValidKeyLength(keyLength) {
+		yellowConsole.Println(
+			"incorrect key length. default key length",
+			rsa.DefaultKeyLength,
+			"bits will be used",
+		)
 		keyLength = rsa.DefaultKeyLength
 	}
 
 	args.KeyLength = keyLength
-
-	if *publicKeyFile == "" {
-		*publicKeyFile = "public_" + getRandomString(16) + ".txt"
-	}
-
-	if *privateKeyFile == "" {
-		*privateKeyFile = "private_" + getRandomString(16) + ".txt"
-	}
 
 	args.PublicKeyPath = *publicKeyFile
 	args.PrivateKeyPath = *privateKeyFile
 
 	args.PrivateKeyPassword = *privateKeyPassword
 
-	arr := strings.SplitN(*cmd, " ", 2)
-	args.Command = arr[0]
-	args.Message = arr[1]
+	args.Command = *command
+	args.Message = *message
 
 	return
 }
